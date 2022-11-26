21a05bdcc01a1ab89d79cb07dd343c763746b7a9
guisantos13 <ghs23.06@hotmail.com>
2022-11-26 16:38:06 -0300

    creation project structure

---
+++ raw/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  default = "dev"
+}
+variable "prefix" {
+  default = "eBird"
+}
+variable "bucket_data_engineering_prefix" {
+  default = "data-engineering"   
+}
+variable "user_name" {
+  default = "gsantos"  
+}
