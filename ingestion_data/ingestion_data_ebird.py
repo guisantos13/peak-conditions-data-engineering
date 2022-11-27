72e0bc6ad955803de7811f9786ffa315cac669f6
guisantos13 <ghs23.06@hotmail.com>
2022-11-27 16:10:42 -0300

    adjust continuous integration to ebird project

--- ingestion_data/ingestion_data_ebird.py
+++ ingestion_data/ingestion_data_ebird.py
@@ -1,2 +1,14 @@
-def soma(x, y):    
+"""Função responsavél pelo consumo da API eBird."""
+
+
+def soma(x, y):
+    """_summary_ .
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return x+y
